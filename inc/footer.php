1544f41a6efe5426decf6c930b5caa291c0aaf4d
Sanjeevini Cholin <cholinsanjeevini@gmail.com>
2025-05-29 01:31:37 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,44 @@
+<div class="container-fluid bg-white mt-5">
+		<div class="row">
+			<div class="col-lg-4 p-4">
+				<h5 class="mb-2" >Get In Touch</h5>
+				
+				<i class="bi bi-geo-alt-fill"></i>Hassan, Belagavi, Bengaluru<br>
+				<a href="tel: +917020464064" class="d-inline-block mb-2 mt-2 text-decoration-none text-dark">
+					<i class="bi bi-telephone-fill"></i>+917020464064
+				</a><br>
+				<a href="mailto: ajjuisbot@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+					<i class="bi bi-envelope-fill"></i>officialzolo@gmail.com
+				</a>
+				
+			</div>
+			<div class="col-lg-4 p-4">
+				<h5 class="mb-2">Quick Links</h5>
+				<a href="index.php" class="d-inline-block mb-2 mt-2 text-decoration-none text-dark">
+					Accommodation
+				</a><br>
+				<a href="rooms.php" class="d-inline-block mb-2 text-decoration-none text-dark">
+					Rooms
+				</a><br>
+				<a href="customers.php" class="d-inline-block mb-2 text-decoration-none text-dark">
+					Our Customers
+				</a><br>
+				<a href="about.php" class="d-inline-block mb-2 text-decoration-none text-dark">
+					About Zolo
+				</a><br>
+				<a href="https://www.mcealumni.in/site/privacy.dz" class="d-inline-block mb-2 text-decoration-none text-dark">
+					Privacy Policy
+				</a><br>
+				<a href="https://www.mcehassan.ac.in/home/Initiative/299" class="d-inline-block mb-2 text-decoration-none text-dark">
+					Terms & Condition
+				</a>
+			</div>
+			<div class="col-lg-4 p-4">
+				<h5 class="mb-2">Newsletter</h5>
+				<p>Don't miss out on the latest updates and exclusive offers! Subscribe to our newsletter today to stay informed and make the most out of your PG living experience. Simply visit our website and sign up with your email address to join our mailing list.</p>
+			</div>
+		</div>
+</div>
+<h6 class="text-center bg-light text-dark p-3 m-0 "><img src="images/test/icon.png" width="27px" height="27px"> Designed & Developed by Chinmay, Chandan, Deshik & Chethan</h6>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
